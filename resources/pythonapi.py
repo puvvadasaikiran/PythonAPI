d4f4974b91bbbfb255874d1a7ba973c4b4e46237
puvvadasaikiran <30699361+puvvadasaikiran@users.noreply.github.com>
2020-01-13 13:55:33 +0530

    Update pythonapi.py

--- resources/pythonapi.py
+++ resources/pythonapi.py
@@ -26,3 +26,22 @@ class PythonAPI(Resource):
 
     #return output
     return(incomerange) , 200
+
+  def put(self, id):
+    values = val.split("_")
+
+    #reshape values
+    valarray=np.asarray(values[:10])
+    valarray=valarray.reshape(10,1).T
+
+    #scale values
+    scaled_values=con.scaler_pkl.transform(valarray)
+    #predict output
+    output=con.svc_pkl.predict(scaled_values)
+
+    #convert output into categorical variable
+    incomerange=Income[output[0]]
+
+    #return output
+    return(incomerange) , 200
+    
