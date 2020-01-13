8e46cdef7383d541106b9af2fd7c4387e001d446
puvvadasaikiran <30699361+puvvadasaikiran@users.noreply.github.com>
2020-01-13 15:02:20 +0530

    Update pythonapi.py

--- resources/pythonapi.py
+++ resources/pythonapi.py
@@ -1,14 +1,29 @@
 from flask_restful import Resource
-import constant as con
+#import constant as con
 import numpy as np
 from sklearn import preprocessing 
 import pickle
 from sklearn.svm import SVC 
 
 
+
+
 Income=[{"result" :'<=50K'}, {"result":'>50K'}]
 
 class PythonAPI(Resource):
+
+  def __init__(self):
+    with open("scaler.pkl", 'rb') as file1:
+    #scaler_pkl = pickle.load(file1)
+    SCALER_PKL = pickle.load(file1)
+
+
+#predict output
+from sklearn.svm import SVC 
+with open("adultincome.pkl", 'rb') as file2:
+    #svc_pkl = pickle.load(file2)
+    SVC_PKL = pickle.load(file2)
+
   def get(self, val):
     values = val.split("_")
 
@@ -17,9 +32,9 @@ class PythonAPI(Resource):
     valarray=valarray.reshape(10,1).T
 
     #scale values
-    scaled_values=con.SCALER_PKL.transform(valarray)
+    scaled_values=SCALER_PKL.transform(valarray)
     #predict output
-    output=con.SVC_PKL.predict(scaled_values)
+    output=SVC_PKL.predict(scaled_values)
 
     #convert output into categorical variable
     incomerange=Income[output[0]]
@@ -35,9 +50,9 @@ class PythonAPI(Resource):
     valarray=valarray.reshape(10,1).T
 
     #scale values
-    scaled_values=con.SCALER_PKL.transform(valarray)
+    scaled_values=SCALER_PKL.transform(valarray)
     #predict output
-    output=con.SVC_PKL.predict(scaled_values)
+    output=SVC_PKL.predict(scaled_values)
 
     #convert output into categorical variable
     incomerange=Income[output[0]]
