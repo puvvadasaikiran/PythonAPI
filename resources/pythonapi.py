c03189dd1960237f348f84b5327587fbe1b005e3
puvvadasaikiran <30699361+puvvadasaikiran@users.noreply.github.com>
2020-01-14 22:47:46 +0530

    Update pythonapi.py

--- resources/pythonapi.py
+++ resources/pythonapi.py
@@ -19,11 +19,6 @@ class PythonAPI(Resource):
 
     Income=[{"result" :'<=50K'}, {"result":'>50K'}]
 
-
-    with open("/app/resources/scaler.pkl", 'rb') as file1:
-    #scaler_pkl = pickle.load(file1)
-      SCALER_PKL = pickle.load(file1)
-
     with open("/app/resources/adultincome.pkl", 'rb') as file2:
     #svc_pkl = pickle.load(file2)
       SVC_PKL = pickle.load(file2)
@@ -34,10 +29,9 @@ class PythonAPI(Resource):
     valarray=np.asarray(values[:10])
     valarray=valarray.reshape(10,1).T
 
-    #scale values
-    scaled_values=SCALER_PKL.transform(valarray)
+  
     #predict output
-    output=SVC_PKL.predict(scaled_values)
+    output=SVC_PKL.predict(valarray)
 
     #convert output into categorical variable
     incomerange=Income[output[0]]
